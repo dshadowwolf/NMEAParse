f8ba49e6d58f9187780fe39539d1d3d7e70c2bf3
Daniel Hazelton <madman@keil-draco.com>
2014-11-19 16:41:29 -0500

    Finally write some documentation

--- lib/NMEAParse.pm
+++ lib/NMEAParse.pm
@@ -12,6 +12,53 @@ use Carp qw/carp croak/;
 use Module::Loader;
 use Class::Method::Modifiers qw/fresh/;
 use Readonly;
+=head1 NAME
+
+NMEAParse - Parse NMEA 0183 compliant sentences
+
+=head1 SYNOPSIS
+
+  use NMEAParse;
+  my $parser = NMEAParse->new( ... );
+  $parser->parse( ... );
+  
+=head1 DESCRIPTION
+
+NMEAParse is an extensible parser for NMEA 0183 sentences. Each sentence
+is represented by a sub-module named after the type field of the sentence
+that can be loaded at parser creation by telling the parser that you expect that
+sentence.
+
+This module exists because GPS::NMEA expects to handle all IO internally and
+in a blocking manner. Since there are situations where the IO is going to be
+done elsewhere manually or where the programmer has decided to do the IO
+asynchronously, this module was born.
+
+The actual interpretation of the data is handled by plugin-like modules that are
+in the NMEAParse namespace (or directory) somewhere in @INC and must
+have function named 'process' available.
+
+=head2 Methods
+
+=over
+
+=item NMEAParse->new( ... )
+
+Creates a new NMEAParse object. The arguments are a list of the types of
+NMEA sentences that the parser should be prepared to handle.
+
+The current list of NMEA sentences the parser has handlers for are:
+  GGA
+  GLL
+  GSA
+  GSV
+  RMC
+  VTG
+  ZDA
+
+=back
+
+=cut
 
 sub new {
     my ( $class, @wanted ) = @_;
@@ -38,9 +85,35 @@ sub new {
     return $self;
 }
 
+=over
+
+=item NMEAParse::checksum( $sentence_to_checksum )
+
+This function is used internally to validate the checksum of an input NMEA
+sentence. To use it, pass in the NMEA sentence without the leading sigil and
+it will return the value you can then properly append to the NMEA sentence.
+
+=back
+
+=cut
+
 sub checksum {
     return sprintf '%02X', ord reduce { $a ^ $b } split //sxm, shift;
 }
+
+=over
+
+=item NMEAParse::parse( $sentence_to_parse )
+
+NMEAParse::parse is the interface to actually parsing the NMEA sentences. The
+C<sentence_to_parse> argument is the complete NMEA 0183 sentence (trailing
+carriage return and newline optional). This function does not return any value,
+instead the parsed data is stored in an internal structure that can be later
+de-referenced.
+
+=back
+
+=cut
 
 sub parse {
     Readonly my $CSUM_STARTS       => 3;
@@ -85,12 +158,34 @@ sub parse {
     return;
 }
 
+=over
+
+=item NMEAParse::get_position
+
+Returns the raw NMEA data of the current location. If this data is unavailable
+it will be replaced with the empty string.
+
+=back
+
+=cut
+
 sub get_position {
     my $self = shift;
     my $d    = $self->{DATA};
 
     return ( $d->{lat_NS}, $d->{latitude}, $d->{lon_EW}, $d->{longitude} );
 }
+
+=over
+
+=item NMEAParse::get_time
+
+Returns the current UTC time as parsed from the passed in NMEA data. If this
+data is unavailable, it will return the empty string.
+
+=back
+
+=cut
 
 sub get_time {
     my $self = shift;
@@ -99,6 +194,18 @@ sub get_time {
     return $d->{time_utc};
 }
 
+=over
+
+=item NMEAParse::get_heading
+
+This function returns the current 'true' heading (as opposed to magnetic) that 
+has been parsed from the NMEA data. Should this data be unavailable, the
+empty string will be returned.
+
+=back
+
+=cut
+
 sub get_heading {
     my $self = shift;
     my $d    = $self->{DATA};
@@ -106,11 +213,51 @@ sub get_heading {
     return $d->{course_true};
 }
 
+=over
+
+=item NMEAParse::get_speed
+
+Get the current reported velocity of the GPS reciever, in kilometers per hour,
+as parsed from the NMEA data. Should this be unavailable, an empty string
+will be returned.
+
+=back
+
+=cut
+
 sub get_speed {
     my $self = shift;
     my $d    = $self->{DATA};
 
     return $d->{speed_in_kph};
 }
+
+=head1 BUGS
+
+Due to the newness of this code, there are no known bugs. To report bugs or to provide a patch or failing test, please see the github repository for this code.
+
+=head1 TODO
+
+=over
+
+=item Expand the number of known NMEA 0183 sentences
+
+=item Expand the documentation to be much better
+
+
+=back
+
+=head1 AUTHORS
+Daniel Hazelton <dshadowwolf@gmail.com>
+
+some contributions from others
+
+=head1 COPYRIGHT
+
+Copyright (c) 2014 Daniel Hazelton. All rights reserved. This program is free
+software; you can redistribute it and/or modify it under the same terms as Perl
+itself.
+
+=cut
 
 1;
